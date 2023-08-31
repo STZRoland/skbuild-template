03169ef6828237a55eadc640e93257c591496274
Roland Stolz <roland.stolz@tum.de>
2023-08-31 10:54:22 +0200

    Fixed the sum_of_list() function.

--- interface/python/python_interface.cpp
+++ interface/python/python_interface.cpp
@@ -11,12 +11,9 @@ namespace py = pybind11;
 PYBIND11_MODULE(skbuild_template, m) {
     m.doc() = "An example Python module build with skbuild.";
 
-    // TODO: This does not work yet!
     m.def("sum_of_list", &sum_of_list, "A function which computes the sum of a vector.");
 
     m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
         Subtract two numbers
-
-        Some other explanation about the subtract function.
     )pbdoc");
 }
